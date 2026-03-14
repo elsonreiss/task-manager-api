ce7f577b43defa548febd451d7930da25470a2f4
Elson Reis <elsonreis084@gmail.com>
2026-03-14 17:55:09 -0300

    feat: add board entity with user relationship

---
+++ src/main/java/com/elsonreis/tesk_manager/entity/Board.java
@@ -0,0 +1,32 @@
+package com.elsonreis.tesk_manager.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Entity
+@Table(name = "tb_boards")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O nome do board é obrigatório")
+    @Size(max = 100, message = "O nome do board deve ter no máximo 100 caracteres")
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "board_id", nullable = false)
+    private User user;
+}
