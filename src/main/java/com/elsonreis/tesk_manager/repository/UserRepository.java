bf2d5df5b073a1d0736642e7e5431a3be136564e
Elson Reis <elsonreis084@gmail.com>
2026-03-14 13:26:47 -0300

    feat: add user repository

---
+++ src/main/java/com/elsonreis/tesk_manager/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.elsonreis.tesk_manager.repository;
+
+import com.elsonreis.tesk_manager.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
+}
