37a899a28b61bcb6fa124122e4723a8c8aca1dfc
Elson Reis <elsonreis084@gmail.com>
2026-03-14 19:49:52 -0300

    feat: add board repository

---
+++ src/main/java/com/elsonreis/tesk_manager/repository/BoardRepository.java
@@ -0,0 +1,14 @@
+package com.elsonreis.tesk_manager.repository;
+
+import com.elsonreis.tesk_manager.entity.Board;
+import com.elsonreis.tesk_manager.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    List<Board> findByUser(User user);
+
+    List<Board> findByUserId(Long userId);
+}
