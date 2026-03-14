d95519633184955d04047ef694f24703c655a65d
Elson Reis <elsonreis084@gmail.com>
2026-03-14 13:04:29 -0300

    chore: initial project setup with Spring Boot

---
+++ src/main/java/com/elsonreis/tesk_manager/TeskManagerApplication.java
@@ -0,0 +1,13 @@
+package com.elsonreis.tesk_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeskManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeskManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elsonreis/tesk_manager/TeskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elsonreis.tesk_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
