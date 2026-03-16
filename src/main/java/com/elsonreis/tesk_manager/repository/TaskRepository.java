8fbf7a5ee05ee364c2430542b40f4201c8e0edfb
Elson Reis <elsonreis084@gmail.com>
2026-03-16 16:54:26 -0300

    feat: add task repository

---
+++ src/main/java/com/elsonreis/tesk_manager/repository/TaskRepository.java
@@ -0,0 +1,13 @@
+package com.elsonreis.tesk_manager.repository;
+
+import com.elsonreis.tesk_manager.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    List<Task> findByBoardId(Long boardId);
+
+    List<Task> findByStatus(String status);
+}
