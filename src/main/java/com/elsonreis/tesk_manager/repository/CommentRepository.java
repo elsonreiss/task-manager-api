9b21b356469bf1bb75d3c6d6c759b04fb77c0c84
Elson Reis <elsonreis084@gmail.com>
2026-03-17 18:56:51 -0300

    feat: add comment repository

---
+++ src/main/java/com/elsonreis/tesk_manager/repository/CommentRepository.java
@@ -0,0 +1,13 @@
+package com.elsonreis.tesk_manager.repository;
+
+import com.elsonreis.tesk_manager.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    List<Comment> findByTaskId(Long taskId);
+
+    List<Comment> findByUserId(Long userId);
+}
