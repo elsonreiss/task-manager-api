df628d395f74364ea7457d16f1f5c95458fe95fa
Elson Reis <elsonreis084@gmail.com>
2026-03-17 18:51:59 -0300

    feat: add comment entity

---
+++ src/main/java/com/elsonreis/tesk_manager/entity/Comment.java
@@ -0,0 +1,33 @@
+package com.elsonreis.tesk_manager.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "comments")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Comment content cannot be blank")
+    @Size(max = 500, message = "Comment content cannot exceed 500 characters")
+    @Column(nullable = false, length = 500)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "task_id", nullable = false)
+    private Task task;
+
+
+}
