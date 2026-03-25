8163c1d6f2d6e18720fbe638f2ae5702d3a81c01
Elson Reis <elsonreis084@gmail.com>
2026-03-25 17:54:07 -0300

    fix: finalize authentication response implementation

--- src/main/java/com/elsonreis/tesk_manager/service/AuthService.java
+++ src/main/java/com/elsonreis/tesk_manager/service/AuthService.java
@@ -1,4 +1,36 @@
 package com.elsonreis.tesk_manager.service;
 
+import com.elsonreis.tesk_manager.dto.request.LoginRequest;
+import com.elsonreis.tesk_manager.dto.response.LoginResponse;
+import com.elsonreis.tesk_manager.entity.User;
+import com.elsonreis.tesk_manager.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+
+    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtService jwtService) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtService = jwtService;
+    }
+
+    public LoginResponse login(LoginRequest request) {
+
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado!"));
+
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            throw new RuntimeException("Senha incorreta!");
+    }
+
+        String token = jwtService.generateToken(user.getEmail());
+
+        return new LoginResponse(token);
+    }
 }
