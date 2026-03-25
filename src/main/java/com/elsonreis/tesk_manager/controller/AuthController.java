6798ae29443a39b08d2fd2bef99b0f75ead6ecdb
Elson Reis <elsonreis084@gmail.com>
2026-03-25 18:09:15 -0300

    feat: add login endpoint

---
+++ src/main/java/com/elsonreis/tesk_manager/controller/AuthController.java
@@ -0,0 +1,26 @@
+package com.elsonreis.tesk_manager.controller;
+
+import com.elsonreis.tesk_manager.dto.request.LoginRequest;
+import com.elsonreis.tesk_manager.dto.response.LoginResponse;
+import com.elsonreis.tesk_manager.service.AuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+}
