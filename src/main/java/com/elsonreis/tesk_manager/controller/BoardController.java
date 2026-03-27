ca985029ce2cd4e23cd3c7572eb8700c3a6c2e6f
Elson Reis <elsonreis084@gmail.com>
2026-03-27 19:27:13 -0300

    refactor(board): extract logged user email from SecurityContext in controller

--- src/main/java/com/elsonreis/tesk_manager/controller/BoardController.java
+++ src/main/java/com/elsonreis/tesk_manager/controller/BoardController.java
@@ -2,6 +2,7 @@ package com.elsonreis.tesk_manager.controller;
 
 import com.elsonreis.tesk_manager.dto.request.BoardRequest;
 import com.elsonreis.tesk_manager.dto.response.BoardResponse;
+import com.elsonreis.tesk_manager.secutiry.AuthUtils;
 import com.elsonreis.tesk_manager.service.BoardService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
@@ -22,8 +23,8 @@ public class BoardController {
 
     @PostMapping
     public ResponseEntity<BoardResponse> createBoard(@Valid @RequestBody BoardRequest request) {
-
-        BoardResponse createdBoard = service.createBoard(request);
+        String email = AuthUtils.getEmailFromContext();
+        BoardResponse createdBoard = service.createBoard(request, email);
         return ResponseEntity.status(201).body(createdBoard);
     }
 
@@ -50,8 +51,8 @@ public class BoardController {
 
     @PutMapping("/{id}")
      public ResponseEntity<BoardResponse> updateBoard(@PathVariable Long id, @Valid @RequestBody BoardRequest request) {
-
-        BoardResponse updatedBoard = service.updateBoard(id, request);
+        String email = AuthUtils.getEmailFromContext();
+        BoardResponse updatedBoard = service.updateBoard(id, request, email);
         return ResponseEntity.ok(updatedBoard);
     }
 
