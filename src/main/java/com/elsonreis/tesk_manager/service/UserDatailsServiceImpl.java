618f9beaab45c03af60c045a9a800b9b536e1734
Elson Reis <elsonreis084@gmail.com>
2026-03-27 15:18:59 -0300

    feat(auth): implement UserDetails on User entity

---
+++ src/main/java/com/elsonreis/tesk_manager/service/UserDatailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.elsonreis.tesk_manager.service;
+
+import com.elsonreis.tesk_manager.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDatailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDatailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado!"));
+    }
+}
